60e5eb0cae44bce2edbf6d1d0df1403cc1bdbe18
jul1ana <julianaadt97@gmail.com>
2023-02-16 18:19:44 -0300

    Implementing a section for Instagram social network

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Hero from "@/components/Hero";
 import Slider from "@/components/Slider";
 import { SliderData } from "@/components/SliderData";
+import Instagram from "@/components/Instagram";
 
 export default function Home() {
   return (
@@ -17,6 +18,8 @@ export default function Home() {
       <Hero heading="Captur Photography" message="I capture moments in nature and keep them alive." />
 
       <Slider slides={SliderData} />
+
+      <Instagram />
     </>
   );
 }
