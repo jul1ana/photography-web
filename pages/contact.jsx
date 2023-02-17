b858088f89561381c5b7ef67b7a7e26b0fe24799
jul1ana <julianaadt97@gmail.com>
2023-02-17 09:08:02 -0300

    Basic modifications in the Head

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 import Hero from "@/components/Hero";
 import Contact from "@/components/Contact";
@@ -6,6 +7,10 @@ import Contact from "@/components/Contact";
 const contact = () => {
   return (
     <div>
+      <Head>
+        <title>Photography - Contact</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Hero heading="Contact" message="Submit the form below for more work and quotes." />
 
       <Contact />
--- pages/work.jsx
+++ pages/work.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
@@ -6,6 +7,10 @@ import Portfolio from "@/components/Portfolio";
 const work = () => {
   return (
     <div>
+      <Head>
+        <title>Photography - Work</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Hero heading="My Work" message="This is some of my recent work traveling the world." />
 
       <Portfolio />
